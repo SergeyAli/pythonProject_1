7346fb5d2cd14293ad4b7333cf654f04e8ddbfa1
SergeyAli <108223744+SergeyAli@users.noreply.github.com>
2023-07-03 15:34:53 +0700

    Update py_home_03.py
    Home_03_03

--- PythonGB/Seminar/01/py_home_03.py
+++ PythonGB/Seminar/01/py_home_03.py
@@ -16,8 +16,7 @@ while count > 0:
     count -= 1
 
     print('Введите число между', LOWER_LIMIT, 'и', UPPER_LIMIT, '!')
-    num_1 = float(input())
-    # if num < LOWER_LIMIT or num > UPPER_LIMIT:
+    num_1 = float(input())    
     if num != num_1:
          if num > num_1:
             print('Задуманное число больше чем: ', num_1)
