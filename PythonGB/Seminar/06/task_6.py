bb78e4e6bde70bfd3eb6b104f67ff840e5a49b2c
Sergey <aliger99@rambler.ru>
2023-07-20 14:46:23 +0700

    Семинар_06_1

--- PythonGB/Seminar/06/task_6.py
+++ PythonGB/Seminar/06/task_6.py
@@ -8,3 +8,32 @@
 Для формирования результатов используйте генераторное выражение.
 '''
 
+
+from task_4 import game_02
+
+_dict_log = dict()
+
+
+def multy_game():
+    dict_all = {'Зимой и летом одним цветом': ['ель', 'ёлка', 'сосна'],
+                'Не лает, не кусает, в дом не пускает': ['замок'],
+                'Сидит дед во сто шуб одет': ['лук', 'луковица']}
+
+    for question, answers in dict_all.items():
+        res = game_02(question, answers)
+        add_dict_log(question, res)
+
+
+def add_dict_log(quesestion: str, answer: int):
+    _dict_log[quesestion] = answer
+
+def print_result():
+    result = (f'Загадка: {key} - результат - {"Не угадали" if value == 0 else f"Угадалди с {value} попытки"}' for key, value
+              in _dict_log.items())
+
+    print('\n'.join(result))
+
+
+if __name__ == '__main__':
+    multy_game()
+    print_result()
