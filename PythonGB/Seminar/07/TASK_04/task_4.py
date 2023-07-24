5181a56dbe7c2a93ee21d79dc731e202341266ed
Sergey <aliger99@rambler.ru>
2023-07-24 14:57:10 +0700

    Семинар_07_0

--- PythonGB/Seminar/07/TASK_04/task_4.py
+++ PythonGB/Seminar/07/TASK_04/task_4.py
@@ -12,3 +12,27 @@
 '''
 
 
+
+import random
+from random import sample, randint
+
+MIN_LEN_NAME = 6
+MAX_LEN_NAME = 30
+MIN_SIZE = 256
+MAX_SIZE = 4096
+COUNT_FILE = 42
+STR_CHAR = 'qwrtpsdfghjklzxcvbnmeyuioa'
+
+
+def create_file(exp: str, min_len_name=MIN_LEN_NAME, max_len_name=MAX_LEN_NAME, min_size=MIN_SIZE, max_size=MAX_SIZE,
+                count_file=COUNT_FILE):
+    for _ in range(0, count_file):
+        name_file = "".join(random.choices(STR_CHAR, k=random.randint(min_len_name, max_len_name))) + '.' + exp
+
+        with open(name_file, 'wb') as f:
+            f.write(bytes(randint(0, 255) for _ in range(randint(min_size, max_size))))
+
+
+if __name__ == '__main__':
+    create_file('txt', count_file=3)
+
