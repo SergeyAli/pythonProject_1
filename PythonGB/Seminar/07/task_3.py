cc3c262ea2dbf6c47d271cb5eb45f2b0959bafb3
Sergey <aliger99@rambler.ru>
2023-07-24 14:50:21 +0700

    Семинар_07_0

--- PythonGB/Seminar/07/task_3.py
+++ PythonGB/Seminar/07/task_3.py
@@ -8,24 +8,32 @@
 ✔ При достижении конца более короткого файла, возвращайтесь в его начало.
 '''
 
-import random
-from random import randint, uniform, shuffle
 
-MIN_LEN_PS = 4
-MAX_LEN_PS = 7
-STR_VOWEL = 'eyuioa'
-STR_CHAR = 'qwrtpsdfghjklzxcvbnm'
-MIN_NUM = -1000
-MAX_NUM = 1000
+def create_file(file_nums: str, file_psew: str, file_res: str):
+    with (
+        open(file_nums, 'r', encoding='utf-8') as f_nums,
+        open(file_psew, 'r', encoding='utf-8') as f_psew,
+        open(file_res, 'w', encoding='utf-8') as f_res
+    ):
+        f_psew_len = len(f_psew.readlines())
+        f_nums_len = len(f_nums.readlines())
+
+        for i in range(0, max(f_nums_len, f_psew_len)):
+            spam = f_nums.readline().strip()
+            eggs = f_psew.readline().strip()
+            if not spam:
+                f_nums.seek(0)
+                spam = f_nums.readline().strip()
+            if not eggs:
+                f_psew.seek(0)
+                eggs = f_psew.readline().strip()
+
+            curr_list = spam.split('|')
+            mult = int(curr_list[0]) * float(curr_list[1])
+
+            f_res.write(f'{eggs.lower()} {abs(mult)}\n' if mult < 0 else f'{eggs.upper()} {round(mult)}\n')
 
 
-def gen_psevdonim(name_file: str):
-    spam = random.sample(STR_CHAR, randint(MIN_LEN_PS, MAX_LEN_PS - 1))
-    eggs = random.sample(STR_VOWEL, randint(1, len(STR_VOWEL)))
-    eggs.extend(spam)
-    eggs = eggs[:random.randint(MIN_LEN_PS, MAX_LEN_PS)]
-    random.shuffle(eggs)
-    print(eggs)
-    with open(name_file, 'a', encoding='utf-8') as f:
-        f.write(f'{"".join(eggs).title()}\n')
 
+if __name__ == '__main__':
+    create_file('nums.txt', 'psew.txt', 'result.txt')
